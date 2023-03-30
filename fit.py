270bc3bd8ab7984c8dcc5173405ed989f95c1a74
streamlet <streamlet@outlook.com>
2023-03-31 02:41:06 +0800

    add

---
+++ fit.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+import os, sys
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+
+
+def cv2read(path):
+    return cv2.imdecode(np.fromfile(path, dtype=np.uint8), -1)
+
+
+def load(path):
+    images = []
+    labels = []
+    for label in os.listdir(path):
+        label_dir = os.path.join(path, label)
+        if os.path.isdir(label_dir):
+            for file_name in os.listdir(label_dir):
+                if file_name.endswith('.png'):
+                    image = cv2read(os.path.join(label_dir, file_name))
+                    image_grayed = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+                    images.append(image_grayed)
+                    labels.append(label)
+    return np.array(images), np.array(labels)
+
+
+def train(images, labels):
+    # 将标签进行 one-hot 编码
+    labels_one_hot = tf.keras.utils.to_categorical(labels)
+
+    # 划分训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(images,
+                                                        labels_one_hot,
+                                                        test_size=0.2,
+                                                        random_state=42)
+
+    # 创建模型
+    model = keras.Sequential([
+        keras.layers.Conv2D(32, (3, 3),
+                            activation='relu',
+                            input_shape=(100, 100, 1)),
+        keras.layers.MaxPool2D((2, 2)),
+        keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        keras.layers.MaxPool2D((2, 2)),
+        keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        keras.layers.MaxPool2D((2, 2)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(labels.shape1[1], activation='softmax')
+    ])
+
+    # 编译模型
+    model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
+                  optimizer=keras.optimizers.Adam(lr=0.001),
+                  metrics=['accuracy'])
+
+    # 训练模型
+    model.fit(X_train,
+              y_train,
+              batch_size=32,
+              epochs=10,
+              validation_data=(X_test, y_test))
+
+    # 在测试集上评估模型
+    test_loss, test_acc = model.evaluate(X_test, y_test)
+    print('Test loss:', test_loss, 'test accuracy:', test_acc)
+
+
+def main():
+    if len(sys.argv) < 1:
+        print('usage: fit.py <input_dir>')
+        return
+    img_path = sys.argv[1]
+    images, labels = load(img_path)
+    train(images, labels)
+
+
+if __name__ == '__main__':
+    main()
